dbf9315816058c5baa4b41752b1ced77a0b3d37f
Felipe Cantu Jr <cantujr.felipe@gmail.com>
2023-09-11 16:28:07 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -4,29 +4,28 @@ import Home from './components/Home';
 import Blog from './components/Blog'
 import Portfolio from './components/Portfolio'
 import Contact from './components/Contact';
-import { BrowserRouter as Router, Route, Redirect, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
       <Router>
         <Navbar />
-          <Redirect to="/Home" />
-          <Route path="/Home" component={Home} />
-        <Route path='/Blog'>
-          <Blog />
-        </Route>
-        <Route path='/Blog'>
-          <Blog />
-        </Route>
-        <Route path='/Portfolio'>
-          <Portfolio />
-        </Route>
-        <Route path='/Contact'>
-          <Contact />
-        </Route>
+         <Switch>
+          <Route exact path='/'>
+            <Redirect to='/Home' />
+          </Route>
+          <Route path='/Home' component={Home} />
+          <Route path='/Blog'>
+            <Blog />
+          </Route>
+          <Route path='/Portfolio'>
+            <Portfolio />
+          </Route>
+          <Route path='/Contact'>
+            <Contact />
+          </Route>
+         </Switch>
       </Router>
-    </BrowserRouter>
   );
 }
 
